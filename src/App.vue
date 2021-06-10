d8bc0d2910202a4d27e4593eb0459d6c9f33575c
Sakib Mohammed <sakibmd.cse@gmail.com>
2021-06-11 02:26:49 +0600

    creating a new project & make some components that will use

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </div>
+</template>
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You have to know how to use Google...',
+          link: 'https://google.com'
+        },
+        {
+          id: 'youtube',
+          title: 'YouTube',
+          description: 'Learn lots of things from YouTube',
+          link: 'https://youtube.com'
+        }
+      ]
+    };
+  }
+};
+</script>
